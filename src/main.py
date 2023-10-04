bddd67cdc500ae2b0c4fca8f6449ca60ebfa9c36
galactixx <nwlezien.github@gmail.com>
2023-10-03 19:58:09 -0500

    add initial code

---
+++ src/main.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+from typing import List
+import autopep8
+import difflib
+import click
+import os
+
+from src.mutually_exclusive import MutuallyExclusiveOption
+    
+@click.command()
+@click.option('--py-file',
+              cls=MutuallyExclusiveOption,
+              help='The name of the py file.',
+              mutually_exclusive=['path'],
+              type=click.Path(exists=True))
+@click.option('--path',
+              cls=MutuallyExclusiveOption,
+              help='The name of the path that contains py files.',
+              mutually_exclusive=['py_file'],
+              type=click.Path(exists=True))
+@click.option('--max-line-length',
+              help='The maximum number of characters in a line of code.',
+              type=int,
+              default=79)
+@click.option('--aggresiveness',
+              help='The degree of aggresiveness when formatting code.',
+              type=int,
+              default=1)
+def cli(py_file: str, path: str, max_line_length: int, aggresiveness: int) -> None:
+    diff_list: List[tuple] = []
+    file_list = []
+    if py_file:
+        if Path(py_file).suffix[1:] != 'py':
+            click.echo('Error: File specified is not a py file, please try again and specify a py file.')
+            return
+        file_list.append(py_file)
+    else:
+        py_files = [os.path.join(path, i) for i in os.listdir(path) if 
+                    not os.path.isdir(os.path.join(path, i)) and Path(i).suffix[1:] == 'py']
+        if not file_list:
+            click.echo('Error: There are no py files in specified path, try again and specify path with py files.')
+            return
+        file_list.extend(py_files)
+    for file in file_list:
+        with open(file, 'r') as f:
+            code = f.read()
+        formatted_code = autopep8.fix_code(code, options={'aggressive': aggresiveness,
+                                                          'max_line_length': max_line_length})
+        diff_list.append((code, formatted_code))
+
+        # calculate delta of both files
+        diff = difflib.ndiff(a=code.splitlines(), b=formatted_code.splitlines())
+        with open('C:/Users/12158/Desktop/diff.txt', 'w') as f:
+            for line in diff:
+                f.write(line + '\n')
+
+if __name__ == '__main__':
+    cli()
---
+++ src/mutually_exclusive.py
@@ -0,0 +1,37 @@
+from click import (
+    Option, 
+    UsageError)
+from typing import (
+    Any,
+    List,
+    Tuple)
+
+class MutuallyExclusiveOption(Option):
+    """Class to override the click.Option class to handle mutually exclusive options."""
+    def __init__(self, *args, **kwargs):
+        self.mutually_exclusive = set(kwargs.pop('mutually_exclusive', []))
+        help = kwargs.get('help', '')
+        if self.mutually_exclusive:
+            ex_str = ', '.join(self.mutually_exclusive)
+            kwargs['help'] = help + (
+                ' NOTE: This argument is mutually exclusive with '
+                ' arguments: [' + ex_str + '].'
+            )
+        super(MutuallyExclusiveOption, self).__init__(*args, **kwargs)
+
+    def handle_parse_result(self, ctx, opts, args) -> Tuple[Any, List[str]]:
+        """"""
+        if self.mutually_exclusive.intersection(opts) and self.name in opts:
+            raise UsageError(
+                "Illegal usage: `{}` is mutually exclusive with "
+                "arguments `{}`.".format(
+                    self.name,
+                    ', '.join(self.mutually_exclusive)
+                )
+            )
+
+        return super(MutuallyExclusiveOption, self).handle_parse_result(
+            ctx,
+            opts,
+            args
+        )
